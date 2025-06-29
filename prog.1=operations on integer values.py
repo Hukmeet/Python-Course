c9d52eb83da18aef1bdb571a23f43bd9cb617dfa
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-06-29 15:50:07 +0530

    day_1

---
+++ prog.1=operations on integer values.py
@@ -0,0 +1,9 @@
+print("Add, Subtract, Multiply and Quotient the following :")
+a=21
+b=44
+print(a)
+print(b)
+print("The sum of",a,"and",b,"is:",a+b)
+print("The difference of",a,"and",b,"is:",a-b)
+print("The product of",a,"and",b,"is:",a*b)
+print("The quotient of",a,"and",b,"is:",a/b)
