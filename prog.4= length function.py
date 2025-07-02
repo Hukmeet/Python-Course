2baf8e67207f136d25889db24c232e9c6b2264a5
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-02 12:04:26 +0530

    day_4

---
+++ prog.4= length function.py
@@ -0,0 +1,16 @@
+print("The first value is:")
+s1='Pritee and Hukmeet'
+print(s1)
+print("Its length is:", len(s1))
+print("The second value is:")
+s2='Pritee\
+And\
+Hukmeet'
+print(s2)
+print("Its length is:", len(s2))
+print("The third value is:")
+s3='''Pritee
+And
+Hukmeet'''
+print(s3)
+print("Its length is:", len(s3))
