5665cda8e428200934d5d0ce2ff116aa96231a42
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-03 12:39:31 +0530

    day_5

---
+++ prog.5=biodata.py
@@ -0,0 +1,10 @@
+schl='''<School Name: XYZ High School>'''
+NM=input("Enter the Name of student:... ")
+CLS=input("Enter the Class of student:... ")
+SEC=input("Enter the Section of student:... ")
+DOB=input("Enter the Date Of Birth of student:... ")
+ADD=input("Enter the Address of student:... ")
+print("The biodata of the student is:")
+print(schl)
+print("Name:...",NM,"\t Class:...",CLS,"\t Section:...",SEC,"\tDate \
+Of Birth:...",DOB,"\t Address:...",ADD)
