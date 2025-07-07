f7b74d4c99d6c7d006cf1f0c854a7173714357c1
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-07 12:31:23 +0530

    day_9

---
+++ prog.9=adding any two integers.py
@@ -0,0 +1,5 @@
+print("This program will help you add any two integers")
+a=int(input("Enter the first number to be added:..."))
+b=int(input("Enter the second number to be added:..."))
+sum=a+b
+print("The sum of these two numbers is:",sum)
