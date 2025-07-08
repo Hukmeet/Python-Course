4d471ef63e4e71f75a8a53e45172de5a2bd61b8b
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-08 12:36:06 +0530

    day_10

---
+++ prog.10=adding any three integers.py
@@ -0,0 +1,6 @@
+print("This program will help you add any three integers")
+a=int(input("Enter the first number to be added:..."))
+b=int(input("Enter the second number to be added:..."))
+c=int(input("Enter the third number to be added:...."))
+sum=a+b+c
+print("The sum of these three numbers is:",sum)
