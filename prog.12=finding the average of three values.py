da00be257a60ee5b503428722b3aff47ed313242
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-10 17:30:12 +0530

    day_12

---
+++ prog.12=finding the average of three values.py
@@ -0,0 +1,6 @@
+print("This program will help you find the average of any three values")
+a=float(input("Enter the first value:..."))
+b=float(input("Enter the second value:..."))
+c=float(input("Enter the third value:..."))
+Average=(a+b+c)/3
+print("The average of",a,",",b,"and",c,"is",Average)
