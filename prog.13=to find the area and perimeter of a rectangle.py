4e2c2643621ca454191f74a6b5272e68a1b3e3c3
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-11 17:44:52 +0530

    day_13

---
+++ prog.13=to find the area and perimeter of a rectangle.py
@@ -0,0 +1,7 @@
+print("This program will help you find the Area and Perimeter of a rectangle")
+l=float(input("Enter the Length of the rectangle:..."))
+b=float(input("Enter the Breadth of the rectangle:..."))
+A=l*b
+print("The Area of the rectangle is:",A)
+P=2*(l+b)
+print("The Perimeter of the rectangle is:",P)
