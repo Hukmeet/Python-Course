926f61bc095cc1816302ef4ca956a86e5de7d57c
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-12 12:00:51 +0530

    day_14

---
+++ prog.14= to find the area of circle.py
@@ -0,0 +1,12 @@
+print("This program will help you find the Area of a circle")
+pie=input('''What value of pie do you prefer:
+3.14 or 22/7?''')
+if pie == 3.14:
+          Pie=3.14
+else :
+          Pie=22/7  
+l=int(input("Enter the radius of the circle:..."))
+l=int(l)
+Pie=int(Pie)
+Area=Pie*l*l
+print("The area of circle is:",Area)        
