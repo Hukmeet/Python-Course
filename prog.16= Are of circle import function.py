483b23ff1c74e5cddf9f479d1d5ce5692beebf19
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-14 22:58:18 +0530

    day_16

---
+++ prog.16= Are of circle import function.py
@@ -0,0 +1,7 @@
+import math as m
+print("This program will help you find the Area of a Circle")
+r=float(input("Enter the Radius of the Circle whose area is to be found:..."))
+pie=m.pi
+r2=m.pow(r,2)
+A=pie*r2
+print("The Area of your circle is:",A)
