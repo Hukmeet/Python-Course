2902f19edc7c22bb410075b084bee57f09c93c49
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-20 23:45:03 +0530

    day_22

---
+++ prog.22=tkinter try.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter as tk
+Screen1_HomePage=tk.Tk()
+Screen1_HomePage.title("Know Your Hogwarts House !")
+
+#img=PhotoImage(file="C:\PRITEE & HUKMEET\tk.Screen_1HomePage.PNG")
+b=Button(Screen1_HomePage,text="Welcome to Wizarding World !")
+#,image=img
+b.pack()
+Screen1_HomePage.mainloop()
