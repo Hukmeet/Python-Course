7cacd5a8af06b02abc663c2568571429c1db5f19
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-23 21:37:55 +0530

    day_25

---
+++ prog.25= nos devisible by 3.py
@@ -0,0 +1,5 @@
+import random as R
+print("This program will generate three random integers between 100 & 999 that are devisible by 3 : ...")
+print(R.randrange(102,999,3))
+print(R.randrange(102,999,3))
+print(R.randrange(102,999,3))
