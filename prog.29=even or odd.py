aa06b4e61e7e09f0cc3f9cf37c985164dcefb1e6
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-27 23:22:51 +0530

    day_29

---
+++ prog.29=even or odd.py
@@ -0,0 +1,6 @@
+print("This program will check if a given integer is even or odd.")
+a=int(input("Enter the integer :... "))
+if a%2==0:
+          print(a,'is an even number.')
+else:
+          print(a,'is an odd number.')
