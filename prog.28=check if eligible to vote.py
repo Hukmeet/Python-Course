209828606682a1779218ab9a4cb9c5f21cfd24c3
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-27 23:22:05 +0530

    day_28

---
+++ prog.28=check if eligible to vote.py
@@ -0,0 +1,6 @@
+print("This program will help you find if you are eligible for voting or not.")
+a=int(input('Enter your age (in years) :... '))
+if a:
+      print('Eligible to vote')
+else :
+      print('Not eligible')
