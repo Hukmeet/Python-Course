55f0ac3de4e4c7decbc88f2c093300e0ef2a0d57
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-29 23:43:52 +0530

    day_30

---
+++ prog.30=a is devisible by b or not.py
@@ -0,0 +1,9 @@
+print("This program will help you decide if a number is devisible by the other or not.")
+a=int(input("Enter the first number :... "))
+b=int(input("Enter the second number :... "))
+if a%b==0 :
+          print("Yes, the first number,",a,",is completely devisible by the secon\
+d number,",b,".")
+else:
+          print("No, the first number,",a,",is not devisible by the secon\
+d number,",b,".")
