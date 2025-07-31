a8f631a350d6149e2543524dcca782fc93618838
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-31 20:58:58 +0530

    day_31

---
+++ prog.31=leep year or not.py
@@ -0,0 +1,6 @@
+print("This program will check is the year is a leap year or not")
+year=int(input("Enter the year (in four digits) :... "))
+if year%4==0:
+          print(year,"is a leap year.")
+else:
+          print(year,"is not a leap year.")
