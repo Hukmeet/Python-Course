34c605f0b5d18ab72a5f19f88c2c7cbfb5e92589
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-08-04 23:49:45 +0530

    day_34

---
+++ prog.34= +ve -ve or 0 (not complete yet).py
@@ -0,0 +1,9 @@
+print("This program will help you decide \
+if a given integer is positive, negative or zero.")
+a=int(input("Enter the integer :... "))
+if a>0:
+          print("The given integer is Positive.")
+elif a<0:
+          print("The given integer is Negative.")
+else:
+          print("The given integer is neither Positive nor Negative.")
